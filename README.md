252e4f6413fe78df6cdbb44900435a2d4d7c0a94
Predrag Nikolic <predrag.nikolic@codetri.be>
2019-07-27 12:19:56 +0200

    👌 IMPROVE: Simplify installing of the package

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # LSP-html
 HTML support for Sublime's LSP plugin.
 
-1. Install [LSP](https://packagecontrol.io/packages/LSP) and `LSP-html` from Package Control.
-2. From the command palette run `LSP: Setup HTML server`.
-3. Restart Sublime.
-
-Server related issues report [here](https://github.com/vscode-langservers/vscode-html-languageserver).
-
-> Note: On Mac, if you have problems running `LSP: Setup HTML server`, install [Fix Mac Path](https://packagecontrol.io/packages/Fix%20Mac%20Path), and optionally set the `additional_path_items` in your user preferences.
+* Install [LSP](https://packagecontrol.io/packages/LSP) and `LSP-html` from Package Control.
+* Restart Sublime.
--- plugin.py
+++ plugin.py
@@ -1,97 +1,76 @@
 import shutil
-
+import os
 import sublime
-import sublime_plugin
+
 from LSP.plugin.core.handlers import LanguageHandler
 from LSP.plugin.core.settings import ClientConfig, LanguageConfig
 
-default_name = 'html'
-server_package_name = 'vscode-html-languageserver-bin'
+package_path = os.path.dirname(__file__)
+server_path = os.path.join(package_path, 'node_modules', 'vscode-html-languageserver-bin', 'htmlServerMain.js')
 
-default_config = ClientConfig(
-    name=default_name,
-    binary_args=[
-        "html-languageserver",
-        "--stdio"
-    ],
-    tcp_port=None,
-    enabled=True,
-    init_options=dict(),
-    settings=dict(),
-    env=dict(),
-    languages=[
-        LanguageConfig(
-            'html',
-            ['text.html.basic'],
-            [
-                "Packages/HTML/HTML.sublime-syntax",
-                "Packages/PHP/PHP.sublime-syntax"
-            ]
+
+def plugin_loaded():
+    print('LSP-html: Server {} installed.'.format('is' if os.path.isfile(server_path) else 'is not' ))
+
+    if not os.path.isdir(os.path.join(package_path, 'node_modules')):
+        # install server if no node_modules
+        print('LSP-html: Installing server.')
+        sublime.active_window().run_command(
+            "exec", {
+                "cmd": [
+                    "npm",
+                    "install",
+                    "--verbose",
+                    "--prefix",
+                    package_path
+                ]
+            }
         )
-    ]
-)
+        sublime.message_dialog('LSP-html\n\nRestart sublime after the server has been installed successfully.')
 
-# Dependencies that needs to be installed for the server to work
-dependencies = ['node', 'html-languageserver']
+
+def is_node_installed():
+    return shutil.which('node') is not None
 
 
 def is_installed(dependency):
     return shutil.which(dependency) is not None
 
 
-class LspHtmlSetupCommand(sublime_plugin.WindowCommand):
-    def is_visible(self):
-        if not is_installed('node') or not is_installed('html-languageserver'):
-            return True
-        return False
-
-    def run(self):
-        if not is_installed('node'):
-            sublime.message_dialog(
-                "Please install Node.js before running setup."
-            )
-            return
-
-        if not is_installed('html-languageserver'):
-            should_install = sublime.ok_cancel_dialog(
-                "html-languageserver was not in the PATH.\nInstall {} globally now?".format(
-                    server_package_name)
-            )
-            if should_install:
-                self.window.run_command(
-                    "exec", {
-                        "cmd": [
-                            "npm",
-                            "install",
-                            "--verbose",
-                            "-g",
-                            server_package_name
-                        ]
-                    })
-        else:
-            sublime.message_dialog(
-                "{} is already installed".format(server_package_name)
-            )
-
-
 class LspHtmlPlugin(LanguageHandler):
-    def __init__(self):
-        self._name = default_name
-        self._config = default_config
-
     @property
     def name(self) -> str:
-        return self._name
+        return 'lsp-html'
 
     @property
     def config(self) -> ClientConfig:
-        return self._config
+        return ClientConfig(
+            name='lsp-html',
+            binary_args=[
+                "html-languageserver",
+                "--stdio"
+            ],
+            tcp_port=None,
+            enabled=True,
+            init_options=dict(),
+            settings=dict(),
+            env=dict(),
+            languages=[
+                LanguageConfig(
+                    'html',
+                    ['text.html.basic'],
+                    [
+                        "Packages/HTML/HTML.sublime-syntax",
+                        "Packages/PHP/PHP.sublime-syntax"
+                    ]
+                )
+            ]
+        )
 
     def on_start(self, window) -> bool:
-        for dependency in dependencies:
-            if not is_installed(dependency):
-                sublime.status_message('Run: LSP: Setup HTML server')
-                return False
+        if not is_node_installed():
+            sublime.status_message('Please install Node.js for the HTML Language Server to work.')
+            return False
         return True
 
     def on_initialized(self, client) -> None:
