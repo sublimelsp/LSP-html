caed36895914a49f3d4570c75467767fbd44d5dc
Predrag Nikolic <predrag.nikolic@codetri.be>
2019-07-27 23:48:23 +0200

    👌 IMPROVE: No need to restart sublime for plugin to take affect
    Listen when the dependencies are installed, and once they are installed,
    register it LSP.

--- plugin.py
+++ plugin.py
@@ -1,9 +1,13 @@
 import shutil
 import os
 import sublime
+import threading
+import subprocess
 
 from LSP.plugin.core.handlers import LanguageHandler
 from LSP.plugin.core.settings import ClientConfig, LanguageConfig
+from LSP.plugin.core.registry import register_language_handler
+
 
 package_path = os.path.dirname(__file__)
 server_path = os.path.join(package_path, 'node_modules', 'vscode-html-languageserver-bin', 'htmlServerMain.js')
@@ -12,29 +16,52 @@ server_path = os.path.join(package_path, 'node_modules', 'vscode-html-languagese
 def plugin_loaded():
     print('LSP-html: Server {} installed.'.format('is' if os.path.isfile(server_path) else 'is not' ))
 
+    # install the node_modules if not installed
     if not os.path.isdir(os.path.join(package_path, 'node_modules')):
-        # install server if no node_modules
-        print('LSP-html: Installing server.')
-        sublime.active_window().run_command(
-            "exec", {
-                "cmd": [
-                    "npm",
-                    "install",
-                    "--verbose",
-                    "--prefix",
-                    package_path
-                ]
-            }
+        # this will be called only when the plugin gets:
+        # - installed for the first time,
+        # - or when updated on package control
+        logAndShowMessage('LSP-html: Installing server.')
+
+        runCommand(
+            onCommandDone,
+            ["npm", "install", "--verbose", "--prefix", package_path ]
         )
-        sublime.message_dialog('LSP-html\n\nRestart sublime after the server has been installed successfully.')
+
+
+def onCommandDone():
+    logAndShowMessage('LSP-html: Server installation completed.')
+    
+    register_language_handler(LspHtmlPlugin())
+
+
+def runCommand(onExit, popenArgs):
+    """
+    Runs the given args in a subprocess.Popen, and then calls the function
+    onExit when the subprocess completes.
+    onExit is a callable object, and popenArgs is a list/tuple of args that 
+    would give to subprocess.Popen.
+    """
+    def runInThread(onExit, popenArgs):
+        try:
+            subprocess.check_call(popenArgs)
+            onExit()
+        except subprocess.CalledProcessError as error:
+            logAndShowMessage('LSP-html: Error while installing the server.', error)
+        return
+    thread = threading.Thread(target=runInThread, args=(onExit, popenArgs))
+    thread.start()
+    # returns immediately after the thread starts
+    return thread
 
 
 def is_node_installed():
     return shutil.which('node') is not None
 
 
-def is_installed(dependency):
-    return shutil.which(dependency) is not None
+def logAndShowMessage(msg, additional_logs=None):
+    print(msg, '\n', additional_logs) if additional_logs else print(msg)
+    sublime.active_window().status_message(msg)
 
 
 class LspHtmlPlugin(LanguageHandler):
