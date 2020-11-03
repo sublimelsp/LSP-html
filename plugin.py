4ebcdd203148fc20f1644fe85d51ceb52d3015b2
Rafał Chłodnicki <rchl2k@gmail.com>
2020-11-03 19:32:43 +0100

    Install outside of the cache directory

--- plugin.py
+++ plugin.py
@@ -14,3 +14,7 @@ class LspHtmlPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'language-server'
     server_binary_path = os.path.join(server_directory, 'out', 'node', 'htmlServerMain.js')
+
+    @classmethod
+    def install_in_cache(cls) -> bool:
+        return False
