b41afaa4dfddeacf63441f57599372841cfb0e63
Raoul Wols <raoulwols@gmail.com>
2020-12-01 21:59:19 +0100

    Fix: handle editor.action.triggerSuggest

--- plugin.py
+++ plugin.py
@@ -1,5 +1,6 @@
 import os
 from lsp_utils import NpmClientHandler
+import sublime
 
 
 def plugin_loaded():
@@ -17,4 +18,16 @@ class LspHtmlPlugin(NpmClientHandler):
 
     @classmethod
     def install_in_cache(cls) -> bool:
+        return False
+
+    def on_pre_server_command(self, command, done_callback):
+        cmd = command["command"]
+        if cmd == "editor.action.triggerSuggest":
+            session = self.weaksession()
+            if session:
+                view = session.window.active_view()
+                if view:
+                    sublime.set_timeout(lambda: view.run_command("auto_complete"))
+                    done_callback()
+                    return True
         return False
