882ad2e1dd5ef865800ad98835994dc9bf00aabe
Rafal Chlodnicki <rchl2k@gmail.com>
2022-10-29 21:32:21 +0200

    Implement support for html.customData
    Fixes #37

--- plugin.py
+++ plugin.py
@@ -1,6 +1,8 @@
-from LSP.plugin.core.typing import Tuple
-from lsp_utils import NpmClientHandler
-import os
+from .types import CustomDataChangedNotification, CustomDataRequest
+from LSP.plugin import Session
+from LSP.plugin.core.typing import Callable, List
+from lsp_utils import ApiWrapperInterface, NpmClientHandler, request_handler
+from os import path
 
 
 def plugin_loaded():
@@ -14,7 +16,7 @@ def plugin_unloaded():
 class LspHtmlPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = "language-server"
-    server_binary_path = os.path.join(
+    server_binary_path = path.join(
         server_directory,
         "html-language-features",
         "server",
@@ -24,6 +26,29 @@ class LspHtmlPlugin(NpmClientHandler):
     )
 
     @classmethod
-    def minimum_node_version(cls) -> Tuple[int, int, int]:
-        # this should be aligned with VSCode's Nodejs version
-        return (14, 0, 0)
+    def required_node_version(cls) -> str:
+        return '>=14'
+
+    def on_ready(self, api: ApiWrapperInterface) -> None:
+        session = self.weaksession()
+        if not session:
+            return
+        self.resolve_custom_data_paths(session)
+
+    def resolve_custom_data_paths(self, session: Session) -> None:
+        custom_data_paths = session.config.settings.get('html.customData')  # type: List[str]
+        resolved_custom_data_paths = []  # type: List[str]
+        for folder in session.get_workspace_folders():
+            resolved_custom_data_paths.extend([path.abspath(path.join(folder.path, p)) for p in custom_data_paths])
+        session.send_notification(CustomDataChangedNotification.create(resolved_custom_data_paths))
+
+    @request_handler(CustomDataRequest.Type)
+    def on_custom_data_content_async(
+        self, params: CustomDataRequest.Params, respond: Callable[[CustomDataRequest.Response], None]
+    ) -> None:
+        file_path, = params
+        if path.isfile(file_path):
+            with open(file_path, 'r', encoding='utf-8') as fd:
+                respond(fd.read())
+                return
+        respond('')
---
+++ types.py
@@ -0,0 +1,19 @@
+from LSP.plugin import Notification
+from LSP.plugin.core.typing import List, Tuple
+
+
+FilePath = str
+
+
+class CustomDataChangedNotification:
+    Type = 'html/customDataChanged'
+    Params = List[FilePath]
+
+    @classmethod
+    def create(cls, params: Params) -> Notification:
+        return Notification(cls.Type, [params])
+
+class CustomDataRequest:
+    Type = 'html/customDataContent'
+    Params = Tuple[FilePath]
+    Response = str
